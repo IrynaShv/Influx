6944938e586eb9dff017502d7324b5faf0e14233
theTinT <racergill17@hotmail.com>
2015-11-28 11:23:51 -0500

    it does stuff...

--- influx.js
+++ influx.js
@@ -1,49 +1,94 @@
-var map;	// Google map object
+function onChangeHandler(key) {
+    if(key.keyCode == 13) {
+        calculateAndDisplayRoute();
+    }
+};
 
-// Initialize and display a google map
-function Init()
-{
-    // HTML5/W3C Geolocation
-    if ( navigator.geolocation )
-        navigator.geolocation.getCurrentPosition( UserLocation );
-    // Default to Washington, DC
-    else
-        ShowLocation( 38.8951, -77.0367, "Washington, DC" );
+function initMap() {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitude = position.coords.latitude;                    //users current
+            var longitude = position.coords.longitude;                 //location
+            var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+             directionsService = new google.maps.DirectionsService();
+             directionsDisplay = new google.maps.DirectionsRenderer();
+            var mapOptions = {
+                zoom: 15,  //Sets zoom level (0-21)
+                center: coords, //zoom in on users location
+            };
+            map = new google.maps.Map(document.getElementById("map"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
+            directionsDisplay.setMap(map);
+        });
+    } else /*TODOOOOO*/{}
 }
 
-// Callback function for asynchronous call to HTML5 geolocation
-function UserLocation( position )
-{
-    ShowLocation( position.coords.latitude, position.coords.longitude, "This is your Location" );
-}
+function calculateTime(start, end, mode){
 
-// Display a map centered at the specified coordinate with a marker and InfoWindow.
-function ShowLocation( lat, lng, title )
-{
-    // Create a Google coordinate object for where to center the map
-    var latlng = new google.maps.LatLng( lat, lng );
+    var totalDuration = 0;
 
-    // Map options for how to display the Google map
-    var mapOptions = { zoom: 12, center: latlng  };
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: mode
+    };
 
-    // Show the Google map in the div with the attribute id 'map-canvas'.
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
 
-    // Place a Google Marker at the same location as the map center
-    // When you hover over the marker, it will display the title
-    var marker = new google.maps.Marker( {
-        position: latlng,
-        map: map,
-        title: title
+            var legs = response.routes[0].legs;
+            for(var i=0; i<legs.length; ++i) {
+                totalDuration += legs[i].distance.value;
+            }
+            directionsDisplay.setDirections(response);
+            console.log(totalDuration);
+        }
     });
 
-    // Create an InfoWindow for the marker
-    var contentString = "<b>" + title + "</b>";	// HTML text to display in the InfoWindow
-    var infowindow = new google.maps.InfoWindow( { content: contentString } );
-
-    // Set event to display the InfoWindow anchored to the marker when the marker is clicked.
-    google.maps.event.addListener( marker, 'click', function() { infowindow.open( map, marker ); });
+    return totalDuration;
 }
 
-// Call the method 'Init()' to display the google map when the web page is displayed ( load event )
-google.maps.event.addDomListener( window, 'load', Init );
+function calculateAndDisplayRoute() {
+    if (navigator.geolocation) { //Checks if browser supports geolocation
+        navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+            var latitude = position.coords.latitude;                    //users current
+            var longitude = position.coords.longitude;                 //location
+            var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+            var mapOptions = {
+                zoom: 15,  //Sets zoom level (0-21)
+                center: coords, //zoom in on users location
+                mapTypeControl: true, //allows you to select map type eg. map or satellite
+                navigationControlOptions: {
+                    style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+                },
+                mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
+            };
+            map = new google.maps.Map( /*creates Map variable*/ document.getElementById("map"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
+            directionsDisplay.setMap(map);
+            var time = calculateTime(coords,document.getElementById('end').value,google.maps.DirectionsTravelMode.DRIVING).done(console.log(time));
+            console.log(time);
+            //var request = {
+            //    origin: coords,
+            //    destination: document.getElementById('end').value,
+            //    travelMode: google.maps.DirectionsTravelMode.DRIVING
+            //};
+            //
+            //directionsService.route(request, function (response, status) {
+            //    if (status == google.maps.DirectionsStatus.OK) {
+            //        directionsDisplay.setDirections(response);
+            //    }
+            //});response
+        });
+    }
+}
+function toggleVisibility(classString) {
+
+    var elements = document.getElementsByClassName("toggle-" + classString);
+    console.log(elements);
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i].style.display == 'none') {
+            elements[i].style.display = 'block';
+        } else {
+            elements[i].style.display = 'none';
+        }
+    }
+}
